097c3535e33f608b2676d459d14c7732a78c0757
here21 <deletetmr@gmail.com>
2017-01-02 19:46:05 +0800

    事件机制

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,25 @@
 // pages/index/index.js
 Page({
   data:{},
+  onWrapTap: function(event){
+    console.log('wrap click!');
+  },
+  onInnerTextTap: function(event){
+    // catch事件绑定可以阻止冒泡事件向上冒泡
+    wx.redirectTo({
+      url: '/pages/posts/post',
+      success: function(res){
+        // success
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+    console.log('inner click!')
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
